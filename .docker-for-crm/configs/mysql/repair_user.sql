456cee5e39de05cdda678c91831e7a538596627f
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2025-10-07 14:24:22 +0200

    - developing cron part in progress...   - mysql additional users add

--- .docker-for-crm/configs/mysql/repair_user.sql
+++ .docker-for-crm/configs/mysql/repair_user.sql
@@ -6,4 +6,6 @@
 #SET PASSWORD FOR 'root'@'localhost' = PASSWORD('real-root-password-from-crm');
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'gateway' IDENTIFIED BY 'real-root-password-from-crm' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON usr_web1_1_at.* TO 'web1_at'@'%' IDENTIFIED BY 'real-root-password-from-crm';
+GRANT ALL PRIVILEGES ON usr_web1_1_at.* TO 'web6'@'%' IDENTIFIED BY 'real-root-password-from-crm';
+GRANT ALL PRIVILEGES ON usr_web1_1.* TO 'toolmaster'@'%' IDENTIFIED BY 'real-root-password-from-crm';
 FLUSH PRIVILEGES;
